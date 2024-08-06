a6410a0fbfea2bb4e3b2c448e69c6f0cec963597
Thiago Albuquerque <thiago.work.ti@outlook.com>
2024-08-06 18:03:08 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Thiago Albuquerque <thiago.work.ti@outlook.com>

--- src/main/java/list/OperacoesBasicas/ListaTarefa.java
+++ src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -4,71 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefa {
-  //atributo
-  private List<Tarefa> tarefaList;
 
-  public ListaTarefa() {
-    this.tarefaList = new ArrayList<>();
-  }
+    final private List<Tarefa> tarefaList;
 
-  public void adicionarTarefa(String descricao) {
-    tarefaList.add(new Tarefa(descricao));
-  }
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
 
-  public void removerTarefa(String descricao) {
-    List<Tarefa> tarefasParaRemover = new ArrayList<>();
-    if (!tarefaList.isEmpty()) {
-      for (Tarefa t : tarefaList) {
-        if (t.getDescricao().equalsIgnoreCase(descricao)) {
-          tarefasParaRemover.add(t);
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa tarefa : tarefaList) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            }
         }
-      }
-      tarefaList.removeAll(tarefasParaRemover);
-    } else {
-      System.out.println("A lista está vazia!");
+
+        tarefaList.removeAll(tarefasParaRemover);
     }
-  }
 
-  public int obterNumeroTotalTarefas() {
-    return tarefaList.size();
-  }
-
-  public void obterDescricoesTarefas() {
-    if (!tarefaList.isEmpty()) {
-      System.out.println(tarefaList);
-    } else {
-      System.out.println("A lista está vazia!");
+    public int obterNumeroTotalDeTarefas() {
+        return tarefaList.size();
     }
-  }
 
-  public static void main(String[] args) {
-    // Criando uma instância da classe ListaTarefa
-    ListaTarefa listaTarefa = new ListaTarefa();
+    public void obterDescricoesDasTarefas() {
+        System.out.println(tarefaList);
+    }
 
-    // Adicionando tarefas à lista
-    listaTarefa.adicionarTarefa("Comprar leite");
-    listaTarefa.adicionarTarefa("Estudar para o exame");
-    listaTarefa.adicionarTarefa("Fazer exercícios");
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
 
-    // Exibindo o número total de tarefas na lista
-    System.out.println("Você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+        System.out.println("O número total de elementos na lista é: "
+                + listaTarefa.obterNumeroTotalDeTarefas());
 
-    // Exibindo as descrições das tarefas na lista
-    listaTarefa.obterDescricoesTarefas();
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de elementos na lista é: "
+                + listaTarefa.obterNumeroTotalDeTarefas());
 
-    // Removendo uma tarefa da lista
-    listaTarefa.removerTarefa("Trabalhar");
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é: "
+                + listaTarefa.obterNumeroTotalDeTarefas());
 
-    // Exibindo o número total de tarefas na lista após a remoção
-    System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
-
-    // Exibindo as descrições das tarefas atualizadas na lista
-    listaTarefa.obterDescricoesTarefas();
-
-    // Removendo uma tarefa da lista quando a lista está vazia
-    listaTarefa.removerTarefa("Estudar para o exame");
-
-    // Exibindo o número total de tarefas na lista após tentar remover de uma lista vazia
-    System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
-  }
+        listaTarefa.obterDescricoesDasTarefas();
+    }
 }
